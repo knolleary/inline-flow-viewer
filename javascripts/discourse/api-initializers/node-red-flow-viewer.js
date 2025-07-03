7fb21c11696530e262cc3b21b4a3b384df30b786
Nick O'Leary <nick.oleary@gmail.com>
2025-07-03 15:13:12 +0100

    Add flow rendered library as an asset

--- javascripts/discourse/api-initializers/node-red-flow-viewer.js
+++ javascripts/discourse/api-initializers/node-red-flow-viewer.js
@@ -1,13 +1,20 @@
+import loadScript from "discourse/lib/load-script"
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("1.14.0", (api) => {
+export default apiInitializer("1.14.0", async (api) => {
+  console.log('loading flow viewer');
+  await loadScript(settings.theme_uploads_local.flow_renderer)
+  console.log('loading flow viewer - done');
+  
   const decorateLines = (post) => {
     try {
       const elems = post.querySelectorAll('code.lang-flows:not(.flows-decorator)');
       elems.forEach(elem => {
 
         elem.parentElement.classList.add('flows-decorator');
-        elem.innerHTML = 'something here!'
+        console.log(elem.innerHTML)
+
+        elem.innerHTML = 'Getting closer...\n\n' + elem.innerHTML;
       });
     } catch (e) {
       console.error(e);
