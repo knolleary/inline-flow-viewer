8f2491d70583b84817515156c0682e13bda533ac
Nick O'Leary <nick.oleary@gmail.com>
2025-07-03 15:35:57 +0100

    Keep iterating

--- javascripts/discourse/api-initializers/node-red-flow-viewer.js
+++ javascripts/discourse/api-initializers/node-red-flow-viewer.js
@@ -1,25 +1,29 @@
 import loadScript from "discourse/lib/load-script"
 import { apiInitializer } from "discourse/lib/api";
 
-export default apiInitializer("1.14.0", async (api) => {
-  console.log('loading flow viewer');
-  await loadScript(settings.theme_uploads_local.flow_renderer)
-  console.log('loading flow viewer - done');
+export default apiInitializer("1.14.0", (api) => {
+  // console.log('loading flow viewer');
+  // await loadScript(settings.theme_uploads_local.flow_renderer)
+  // console.log('loading flow viewer - done');
   
-  const decorateLines = (post) => {
+  const renderFlows = (post) => {
+    console.log('decorateLines called')
     try {
-      const elems = post.querySelectorAll('code.lang-flows:not(.flows-decorator)');
+      const elems = post.querySelectorAll('pre[data-code-wrap=flows]:not(.flows-decorator)');
+      console.log(elems)
       elems.forEach(elem => {
-
-        elem.parentElement.classList.add('flows-decorator');
-        console.log(elem.innerHTML)
-
-        elem.innerHTML = 'Getting closer...\n\n' + elem.innerHTML;
+        elem.classList.add('flows-decorator');
+        const code = elem.querySelector('code');
+        if (code) {
+          console.log(code.innerHTML)
+        } else {
+          console.log('code not found in elem', elem);
+        }
       });
     } catch (e) {
       console.error(e);
     }
   }
   
-  api.decorateCookedElement(decorateLines, {id: 'decorate-flow-viewer'});
+  api.decorateCookedElement(renderFlows, {id: 'decorate-flow-viewer'});
 });
